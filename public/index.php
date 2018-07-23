f239e5b698bb1a3722f9051e0977c10cef511d45
sonclt1992 <son.clt1992@gmail.com>
2018-07-23 20:53:54 +0700

    update code

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <!-- JQUERY SCRIPT -->
+    <SCRIPT src="../js/bootstrap.min.js"></SCRIPT>
+    <SCRIPT src="../js/jquery-master.js"></SCRIPT>
+</body>
+</html>
